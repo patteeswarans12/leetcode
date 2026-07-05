7530b171fe2a248df68a971edd82d208367347b3
Patteeswaran <patteeswarans12@gmail.com>
2026-07-05 20:32:43 +0530

    Time: 7 ms (76.25%), Space: 19.5 MB (63.83%) - LeetHub

---
+++ Backtracking/39. Combination Sum/39. Combination Sum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+
+        def backtrack(start, path, total):
+            if total == target:
+                ans.append(path[:])
+                return
+
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])
+                path.pop()
+
+        backtrack(0, [], 0)
+        return ans
