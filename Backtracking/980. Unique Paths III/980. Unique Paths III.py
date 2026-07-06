6019d24a9e2e5870f1b7da59c62dddfb3e51c770
Patteeswaran <patteeswarans12@gmail.com>
2026-07-06 15:06:11 +0530

    Time: 23 ms (15.47%), Space: 19.4 MB (60.68%) - LeetHub

--- Backtracking/980. Unique Paths III/980. Unique Paths III.py
+++ Backtracking/980. Unique Paths III/980. Unique Paths III.py
@@ -1,35 +1,33 @@
-from typing import List
-
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        total = 0
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] != -1:
-                    total += 1
-                if grid[i][j] == 1:
-                    sx, sy = i, j
-
-        def dfs(x, y, left):
-            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == -1:
-                return 0
-
-            if grid[x][y] == 2:
-                return 1 if left == 1 else 0
-
-            temp = grid[x][y]
-            grid[x][y] = -1
-
-            ans = (
-                dfs(x + 1, y, left - 1) +
-                dfs(x - 1, y, left - 1) +
-                dfs(x, y + 1, left - 1) +
-                dfs(x, y - 1, left - 1)
-            )
-
-            grid[x][y] = temp
-            return ans
-
-        return dfs(sx, sy, total)
+            r=len(grid)
+            n=len(grid[0])
+            s1,s2=0,0
+            c=0
+            for i in range(r):
+                for j in range(n):
+                    if grid[i][j]==1:
+                        s1,s2=i,j
+                    if grid[i][j]!=-1:
+                        c+=1
+            m=0
+            v=[[0]*n for i in range(r)]
+            def solve(i,j,t):
+                nonlocal m,c
+            
+                if i<0 or j<0 or i>=r or j>=n or grid[i][j]==-1 or v[i][j]==1:
+                    return 'false'
+                v[i][j]=1
+            
+                if t==c and grid[i][j]==2:
+                    m+=1
+                    v[i][j]=0
+                    return 'false'
+                solve(i-1,j,t+1)
+                solve(i+1,j,t+1)
+                solve(i,j-1,t+1) 
+                solve(i,j+1,t+1)
+                v[i][j]=0
+                return 'false'
+            solve(s1,s2,1)
+            return m
